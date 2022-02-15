450b0ae5cc9c24ad68d3e851433fe203ef322f2f
Aleksandr Khasanov <sashokhasanov@mail.ru>
2022-02-15 11:47:55 +0300

    Initial Commit

---
+++ TrafficLight/TrafficLightApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightApp.swift
+//  TrafficLight
+//
+//  Created by Сашок on 15.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
